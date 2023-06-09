27498aa839cf6b3a0d287f541c8db0f03a553ca7
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-06-09 16:17:36 -0400

    Final

--- code.js
+++ code.js
@@ -16,49 +16,105 @@ class GoodsList {
     constructor (filter, sortPrice, sortDir) {
         this.filter = filter;
         this.sortPrice = sortPrice;
-        this.sortDir = sortDir;
-        this.#goods = [];
+        this.sortDir = sortDir;        
     }
-    get List () {
-        // Вывести список товаров
+    #goods = []
+    get list () {
+        return this.#goods
+            .filter(good => good.available === true) // фильтр по наличию
+            .filter(good => this.filter.test(good.name)) // фильтр по наименованию
+            .sort((a, b) => (a.price - b.price)*(this.sortDir-0.5)*2) // сортировка по цене со сменой направления
     }
-    add () {
-        // добавть экземпляр товара
+    add (good) {
+        this.#goods.push(good)
     }
     remove (id) {
-        // удалить экземпляр товара
+        this.#goods.splice(id-1,1)
     }
 }
 
+// Создание 10 объектов good
+const goodsList = new GoodsList(/Name/, true, true,)
+
+for (let index = 1; index < 11; index++) {
+    const good = new Good (
+        index,
+        'GoodName'+index,
+        'GoodDescription'+index,
+        ['s', 'm', 'l',],
+        Math.round(Math.random()*100), // рандомная цена
+        );
+    good.setAvailable(Boolean(Math.round(Math.random()))) // рандомное наличие товара
+    goodsList.add(good);
+}
+
 class BasketGood extends Good {
-    constructor (id, name, description, sizes, price, available, amount) {
-        super (id, name, description, sizes, price, available)
+    constructor (Good, amount) {
+        super (Good.id, Good.name, Good.description, Good.sizes, Good.price, Good.available)
         this.amount = amount;
     }
 }
 
 class Basket {
-    constructor (goods) {
-        this.goods = goods;
+    constructor () {
+        this.goods = [];
     }
     get totalAmount () {
-
+        return this.goods.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0,)
     }
     get totalSum () {
-
+        return this.goods.reduce((accumulator, currentValue) => accumulator + (currentValue.price * currentValue.amount), 0,)
+    }
+    data () { // Тест геттеров
+        console.log("Total amount: ", this.totalAmount)
+        console.log("Total sum: ", this.totalSum)
     }
     add (good, amount) {
-
+        let goodId = this.findId(good)
+        if (goodId != -1) {
+            this.goods[goodId].amount += amount
+        } else {
+            const element = new BasketGood (good, amount);
+            this.goods.push(element);  
+        }
     }
     remove (good, amount) {
-
+        let goodId = this.findId(good)
+        if (this.goods[goodId].amount > amount) {
+            this.goods[goodId].amount -= amount
+        } else {
+            this.goods.splice(goodId,1)
+        }
     }
     clear () {
-
+        basket.goods.splice(0, basket.goods.length)
     }
     removeUnavailable () {
-
+        let idDelete = []
+        for (let index = 0; index < basket.goods.length; index++) {
+            if (basket.goods[index].available == false) {
+                idDelete.push(index)
+            };
+        }
+        for (let index = 0; index < idDelete.length; index++) {
+            this.goods.splice(idDelete[index],1);   
+            }
+    }
+    findId (good) {
+        let goodId = -1
+        for (let index = 0; index < this.goods.length; index++) {
+            if (good.id === this.goods[index].id) {
+                goodId = index;
+                break;
+            }   
+        }
+        return goodId
     }
 }
 
-        
+// TEST Корзины (все доступные объекты goods)
+const basket = new Basket()
+for (let index = 0; index < goodsList.list.length; index++) {
+    const element = new BasketGood (goodsList.list[index], index+1);
+    basket.goods.push(element);  
+}
